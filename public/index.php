adc2b917a8742b897ccee50e3b4e8634bbceb176
frederic zai <frederic.zai@gmail.com>
2023-07-17 14:12:25 +0200

    init config

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Importing the dotenv package
+require_once __DIR__ . '/../vendor/autoload.php';
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Symfony\Component\Dotenv\Dotenv;
